5b2d506e64455c207598615c79bc17d5855a9071
LucasBorges <Lucaspborgezz@gmail.com>
2026-01-23 16:53:50 -0300

    Add data initialization function and structures for yard management

---
+++ dados.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dados.h"
+
+void inicializarDados()
+{
+    caminhoes[0] = (Caminhao){"ABC1234", "TransporteA", "CaminhaoA", "Joao Silva", 10};
+    caminhoes[1] = (Caminhao){"DEF5678", "TransporteB", "CaminhaoB", "Maria Silva", 15};
+    caminhoes[2] = (Caminhao){"GHI9012", "TransporteC", "CaminhaoC", "Carlos Oliveira", 20};
+
+    docas[0] = (Doca){1, CARGA, LIVRE, 20.0};
+    docas[1] = (Doca){2, DESCARGA, LIVRE, 25.0};
+    docas[2] = (Doca){3, CARGA, LIVRE, 30.0};
+
+    operacoes[0] = (Operacao){"OP001", "ABC1234", 1, {0}, {0}, {0}, CARGA, "ProdutoA", 10.0, PENDENTE};
+    operacoes[1] = (Operacao){"OP002", "DEF5678", 2, {0}, {0}, {0}, DESCARGA, "ProdutoB", 15.0, PENDENTE};
+    operacoes[2] = (Operacao){"OP003", "GHI9012", 3, {0}, {0}, {0}, CARGA, "ProdutoC", 20.0, PENDENTE};
+}
---
+++ dados.h
@@ -0,0 +1,7 @@
+#include "structs.h"
+
+Caminhao caminhoes[3]; 
+Doca docas[3];
+Operacao operacoes[3];
+
+void inicializarDados();
