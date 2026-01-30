8abde840494eb8eaaa192b4426329db8f68fa506
Felipe <felipe.morais067@gmail.com>
2026-01-30 04:50:29 -0300

    refactor: clean up code structure and improve menu display

--- dados.h
+++ dados.h
@@ -3,10 +3,15 @@
 
 #include "structs.h"
 
-// Global arrays - actual storage in dados.c
-extern Caminhao caminhoes[5]; 
+// Declaração das variáveis globais
+extern Caminhao caminhoes[5];
 extern Doca docas[5];
 extern Operacao operacoes[5];
+
+// Declaração dos contadores de itens
+extern int totalCaminhoes;
+extern int totalDocas;
+extern int totalOperacoes;
 
 void inicializarDados();
 
